2eced2532f48f1a0a5b20172342c2c7e59f359eb
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-02 10:16:06 -0600

    Create entity.h

---
+++ engine/entity/entity.h
@@ -0,0 +1,40 @@
+/*entity.h*/
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+namespace _component 
+{
+class Entity : public Component {
+public:
+  Entity();
+  ~Entity() = default;
+
+virtual void* CreateEntity( Entity& pEntity ) = 0;
+virtual void* DeleteEntity( Entity& pEntity ) = 0;
+virtual void* SetAlias( Entity& pEntity, const char* pAlias ) = 0;
+
+virtual bool SelectEntity( Entity& pEntity ) = 0;
+virtual bool SelectEntityByAlias( const char* pAlias ) = 0;
+
+Entity& get() {
+  return *this;
+}
+
+const char* getAlias() const {
+  return alias;
+}
+
+bool getStatic() {
+  return bStatic ? 1 : 0;
+}
+
+protected:
+  Vector3 position;
+  const char* alias;
+  bool bStatic; // no physics
+};
+
+}
+
+#endif 
