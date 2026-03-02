b7d07e4839dde68432f9a96afecb1fe6252fc7f5
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-02 12:36:39 -0600

    Update char_str.h

--- engine/string/char_str.h
+++ engine/string/char_str.h
@@ -3,15 +3,43 @@
 #ifndef CHAR_STR_
 #define CHAR_STR_
 
-class CharStr {
+class CharStr : public Component {
 public:
   CharStr();
   ~CharStr() = default;
 
-  char* StrArr[];
+  char* StrArr[]; // def array
+  Component& str_ptr;
 
-  virtual void str_set_array(char* pStr, int n) {
+  // set array
+  void str_set_array(char* pStr, int n) {
     *pStr = StrArr[n];
+  }
+
+  // delete string 
+  void str_del( const char* pStr ) {
+    str_ptr.GetAt( pStr );
+    delete *[] pStr;
+  }
+
+  // increment string
+  void str_inc( const char* pStr ) {
+    str_ptr.GetAt( pStr );
+    *pStr++;
+  }
+
+  // decrement string
+  void str_dec( const char* pStr ) {
+    str_ptr.GetAt( pStr );
+    *pStr--;
+  }
+
+  const char* get_str( const char* pStr ) {
+    str_ptr.GetAt( pStr );
+  }
+
+  void str_zip( const char* pStr ) {
+    *pStr = "*.zip";
   }
 
 };
