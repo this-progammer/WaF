2f77177ca6577395997505215e4f14c2d212bec8
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-02 10:06:54 -0600

    Create mesh.h

---
+++ engine/mesh/mesh.h
@@ -0,0 +1,55 @@
+/*mesh.h*/
+
+#ifndef MESH_H
+#define MESH_H
+
+typedef float MeshVec;
+typedef MeshVec MeshVec3[3];
+typedef MeshVec MeshVec4[4];
+
+namespace component 
+{
+class Mesh : public Component {
+  public:
+    Mesh();
+    ~Mesh() = default;
+
+      Mesh& getMesh() const {
+        return *this;
+      }
+
+      virtual void * CreateMesh(Mesh& pMesh) = 0;
+      virtual void * DestroyMesh(Mesh& pMesh) = 0;
+
+      Mesh* Alloc_Mesh();
+
+      virtual bool SelectMesh(Mesh& pMesh) = 0;
+      virtual bool SelectMesh(const Mesh& pMesh) = 0;
+      virtual bool SelectMesh(Mesh** pMesh) = 0;
+
+      MeshVec3 getMins() {
+        return mesh_mins;
+      }
+
+      MeshVec3 getMaxs() {
+        return mesh_maxs;
+      }
+
+      const char* getAlias() const {
+        return mesh_alias;
+      }
+
+      const auto getPoints() const {
+        return mesh_points;
+      }
+
+  protected:
+    MeshVec3 mesh_mins;
+    MeshVec3 mesh_maxs;
+    const char* mesh_alias;
+    auto mesh_points;
+};
+
+}
+
+#endif
