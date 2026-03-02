8b568ed9115555c91df1bbc2bba77f8c88fa588a
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-02 10:24:20 -0600

    Create model.h

---
+++ engine/model/model.h
@@ -0,0 +1,36 @@
+/*model.h*/
+
+#ifndef MODEL_H
+#define MODEL_H
+
+namespace _component 
+{
+
+class Model : public Component {
+public:
+  Model();
+  ~Model();
+
+virtual void * CreateModel( Model& pModel ) = 0;
+virtual void * DestroyModel( Model& pModel ) = 0;
+virtual void * SetModelAlias( const char* pAlias ) = 0;
+
+virtual bool SelectModel( Model& pModel ) = 0;
+
+Model& get() {
+  return *this;
+}
+
+const char* getAlias() const {
+  return alias;
+}
+
+protected:
+const char* alias;
+Vector3 position;
+int vertices;
+};
+
+}
+
+#endif
