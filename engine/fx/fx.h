cd46010f082bcef20d61e9c1fa89dd452fe0c135
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-02 13:57:25 -0600

    Create fx.h

---
+++ engine/fx/fx.h
@@ -0,0 +1,31 @@
+/*fx.h*/
+
+#ifndef FX_H
+#define FX_H
+
+namespace _component {
+class FX : public Component {
+public:
+  FX();
+  ~FX() = default;
+
+  virtual void* CreateFX( FX& pFX, const Vector& X, const Vector& Y, const Vector& Z ) = 0;
+  virtual void* DestroyFX( FX& pFX ) = 0;
+
+  virtual void GetFXAlias( FX& pFX, const char* pAlias ) = 0;
+
+  FX& get() {
+    return *this;
+  }
+
+  Vector3& getPosition() const {
+    return position;
+  }
+
+protected:
+Vector3 position;
+};
+
+}
+
+#endif
