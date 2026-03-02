e6a7ae6e0d0eff90fb7bb3b84430d9008ee5032d
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-02 13:48:29 -0600

    Update texture.h

--- engine/texture/texture.h
+++ engine/texture/texture.h
@@ -3,7 +3,7 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-class Texture : public Component : public CharStr {
+class Texture : public Component {
 public:
   Texture();
   ~Texture() = default;
@@ -17,6 +17,14 @@ public:
   virtual void SetTextureName( const char* pStr ) = 0;
   virtual void* CreateTexture( Texture& pTexture ) = 0;
   virtual void* CreateTextureName( Texture& pTexture, const char* pStr ) = 0;
+  virtual void GetTextureDirectory( const char* pDir, const char* pExt, int nCount ) = 0;
+
+  Texture& get() {
+    return *this;
+  }
+
+  virtual void SetTextureCoords( MeshVec4 coords ) = 0;
+  virtual void HookTextureDevice( HWND pPanel, HDC pDC ) = 0;
 
 };
 
