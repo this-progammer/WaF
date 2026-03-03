cc4ff4eb40fbd21545ed1b2fea4a5ff2c201bd96
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-03 08:37:44 -0600

    Create npc.h

---
+++ engine/ai/npc.h
@@ -0,0 +1,41 @@
+/*npc.h*/
+
+#ifndef NPC_H
+#define NPC_H
+
+namespace _component {
+
+class Npc : public Component {
+public:
+  Npc();
+  ~Npc() = default;
+
+virtual void* CreateNpcNode( Npc& pNpc, Vector3& position ) = 0;
+virtual void* DestroyNpcNode( Npc& pNpc ) = 0;
+virtual void* MiscNpcPtr( Npc*& pNpc ) = 0;
+
+Npc& get() {
+  return *this;
+}
+
+Vector3 getOrigin() {
+  return origin;
+}
+
+const char* getAlias() const {
+  return alias_name;
+}
+
+Model& getModel() {
+  return dynamic_model;
+}
+
+protected:
+Vector3 origin;
+const char* alias_name;
+Model& dynamic_model;
+};
+
+}
+
+#endif
