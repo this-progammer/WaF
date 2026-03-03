1ff4e56e8309887fe69a41a710c2a3475c874835
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-03 12:31:43 -0600

    Create multiplayer.h

---
+++ core/multiplayer/multiplayer.h
@@ -0,0 +1,52 @@
+/*multiplayer.h*/
+
+#ifndef MULTIPLAYER_H
+#define MULTIPLAYER_H
+
+namespace _core {
+
+class CMultiplayer {
+public:
+  CMultiplayer();
+  ~CMultiplayer() = default;
+
+virtual void* CreateNetwork( CMultiplayer& cMP, const char* pServer, int nLobbyCount, const char* pHost, int nHostId ) = 0;
+virtual void* TerminateNetwork( CMultiplayer& cMP ) = 0;
+
+CMultiplayer& get() {
+  return *this
+}
+
+const char* getServer() const {
+  return server;
+}
+
+int getCount() {
+  return lobbycount;
+}
+
+const char* getHostName() const {
+  return host_name;
+};
+
+const int getHostId() const {
+  return host_id;
+}
+
+virtual void IncrementCount( CMultiplayer& cMP ) = 0;
+virtual void DecrementCount( CMultiplayer& cMP ) = 0;
+virtual void SetPlayerCount( int nCount ) = 0;
+
+virtual void Do_MessageWindow( const char* pCaption, const char* pText, HWND pWindow ) = 0;
+virtual void DoMatch() = 0;
+
+protected:
+const char* server;
+int lobbycount;
+const char* host_name;
+int host_id;
+};
+
+}
+
+#endif
