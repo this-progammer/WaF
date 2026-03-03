e378fbc7807a7263a559882cd9fac17219a62188
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-03 12:35:07 -0600

    Update multiplayer.h

--- core/multiplayer/multiplayer.h
+++ core/multiplayer/multiplayer.h
@@ -13,6 +13,12 @@ public:
 virtual void* CreateNetwork( CMultiplayer& cMP, const char* pServer, int nLobbyCount, const char* pHost, int nHostId ) = 0;
 virtual void* TerminateNetwork( CMultiplayer& cMP ) = 0;
 
+virtual void AddPlayer() = 0;
+virtual void ChangeHost() = 0;
+virtual void KickPlayer() = 0;
+
+const virtual void StartTimer() = 0;
+
 CMultiplayer& get() {
   return *this
 }
@@ -40,11 +46,23 @@ virtual void SetPlayerCount( int nCount ) = 0;
 virtual void Do_MessageWindow( const char* pCaption, const char* pText, HWND pWindow ) = 0;
 virtual void DoMatch() = 0;
 
+virtual void SavePingUsingBrushes() = 0;
+
+float getPing() {
+  return ping;
+}
+
+float getTimer() {
+  return timer;
+}
+
 protected:
 const char* server;
 int lobbycount;
 const char* host_name;
 int host_id;
+float ping;
+float timer;
 };
 
 }
