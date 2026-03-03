548d20b0663bf1f574d2e6c5f9c04aad068c95bc
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-03 13:49:52 -0600

    Create wafapi.h

---
+++ api/wafapi.h
@@ -0,0 +1,19 @@
+/*wafapi.h*/
+
+#include <Windows.h>
+
+#ifndef WAFAPI_
+#define WAFAPI_
+
+#ifdef WAFAPI_
+#define _WIN32
+#endif
+
+#ifndef _WIN32
+#error "WaFAPI Error : Win32API not defined!"
+#endif
+
+
+
+
+#endif
