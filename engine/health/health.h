4271f0782b8569a9023314d7d2462473dfa79468
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-04 11:25:04 -0600

    Update health.h

--- engine/health/health.h
+++ engine/health/health.h
@@ -3,4 +3,17 @@
 #ifndef HEALTH_H
 #define HEALTH_H
 
+constexpr const char* const HEALTH_CLASS("Health");
+
+float f_base_health = 150.0f;
+
+class CHealth {
+public:
+  CHealth();
+  ~CHealth() = default;
+
+  virtual void create_base() = 0;
+
+};
+
 #endif
