8dd962e9095e298f3d9ddb8c5362464484000756
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-04 12:05:23 -0600

    Update health.h

--- engine/health/health.h
+++ engine/health/health.h
@@ -13,8 +13,16 @@ public:
   virtual void* initHealth( CHealth& pHealth, bool bZero ) = 0;
   virtual void* destroyHealth( CHealth& pHealth ) = 0;
 
+CHealth& get() {
+  return *this;
+}
+
+CHealth operator ++( CHealth& pHealth );
+CHealth operator --( CHealth& pHealth );
+
 protected:
 float health = 150.0f;
+bool bDead;
 };
 
 #endif
