521a70be2300b13593f30094aa7cea11067bc9c2
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-04 13:46:31 -0600

    Update select_entity.cpp

--- engine/entity/select_entity.cpp
+++ engine/entity/select_entity.cpp
@@ -2,10 +2,20 @@
 
 #include <entity.h>
 
+// select entity
 bool _component::Entity::SelectEntity( Entity& pEntity ) {
   if( pEntity != NULL ) {
     SELECT_ENTITY( pEntity );
     return true
   }
   return false
+}
+
+// select by alias
+bool _component::Entity::SelectEntityByAlias( const char* pAlias ) {
+  if ( this->alias == pAlias ) {
+    this->SelectEntity( this.get() );
+    return true;
+  }
+ return false;
 }
