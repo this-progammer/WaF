1ced732b30cbfa6bf8075c5dafc7c50b9718f1d8
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-04 13:39:58 -0600

    Update select_entity.cpp

--- engine/entity/select_entity.cpp
+++ engine/entity/select_entity.cpp
@@ -3,5 +3,9 @@
 #include <entity.h>
 
 bool _component::Entity::SelectEntity( Entity& pEntity ) {
-
+  if( pEntity != NULL ) {
+    SELECT_ENTITY( pEntity );
+    return true
+  }
+  return false
 }
