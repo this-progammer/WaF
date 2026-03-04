995c3d1c80141111bdf1d4f7ceb9b4e32641350d
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-04 10:16:11 -0600

    Update node.h

--- engine/node/node.h
+++ engine/node/node.h
@@ -5,6 +5,10 @@
 
 #define SELECT_NODE ( n )( g_ComponentPtr.GetAt( n ) )
 
+#define NODE_PATH 0x01
+#define NODE_SCRIPT 0x02
+#define NODE_ANIMATION 0x03
+
 constexpr const char* const NODE_CLASS("Node");
 
 namespace _component {
@@ -14,7 +18,7 @@ public:
   Node();
   ~Node() = default;
 
-virtual void* CreateNode( Node& pNode, const Vector3& origin ) = 0;
+virtual void* CreateNode( Node& pNode, const Vector3& origin, const int& nType ) = 0;
 virtual void* DestroyNode( Node& pNode ) = 0;
 virtual void SetScript( Node& pNode, const char* script ) = 0;
 virtual void SelectNode( Node& pNode ) = 0;
@@ -25,6 +29,7 @@ Mesh& m_MeshNode;
 Vector3 origin;
 const char* alias_name;
 const char* node_script;
+int node_type;
 };
 
 }
