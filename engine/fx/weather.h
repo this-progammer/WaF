54b72742af0696e629dfd4170661af61553d5469
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-04 12:13:27 -0600

    Create weather.h

---
+++ engine/fx/weather.h
@@ -0,0 +1,30 @@
+/*weather.h*/
+
+#ifndef WEATHER_H
+#define WEATHER_H
+
+constexpr const char* const WEATHER_CLASS("FX::Weather");
+
+#define WEATHER_FX_RAIN 0
+#define WEATHER_FX_SNOW 1
+#define WEATHER_FX_THUNDER 2
+#define WEATHER_FX_DUST 3
+
+class CFXTypeWeather {
+public:
+  CFXTypeWeather();
+  ~CFXTypeWeather() = default;
+
+virtual void* createFxWeather( CFXTypeWeather& pFX, int nType, bool bStatic, const char* pAlias ) = 0;
+virtual void* destroyFxWeather( CFXTypeWeather& pFX ) = 0;
+
+CFXTypeWeather& get() {
+  return *this;
+};
+
+protected:
+int type;
+const char* alias;
+};
+
+#endif
