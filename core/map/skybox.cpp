2e1f4ccc7d5ad93059936f47cd55983b8132ced5
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-04 12:38:06 -0600

    Create skybox.cpp

---
+++ core/map/skybox.cpp
@@ -0,0 +1,38 @@
+/*skybox.cpp*/
+
+#include <skybox.h>
+
+_core::Skybox::Skybox()
+{
+
+};
+
+_core::~Skybox::Skybox()
+{
+
+};
+
+// Create Skybox
+void* _core::Skybox::CreateSkybox( Skybox& skybox, const Vector3& scalar, const Vector4& cube, const char* pShader, const Texture& pTexture ) {
+  std::size_t pSz = INT_CSG_HOLLOW;
+  Component& pGet;
+  skybox = ( Skybox** )malloc( sizeof( &skybox ) );
+  for ( auto& i : pSz; pSz++ ) {
+    // make scale
+    pGet.GetAt( scalar );
+    CSG_MAKE_SCALE( scalar );
+
+    // make cube
+    pGet.GetAt( cube );
+    CSG_MAKE_HOLLOW( cube );
+
+    // get shader
+    pGet.GetStr( pShader );
+    STREAM_PROJECT_SHADER( pShader );
+
+    // get texture and set
+    SELECT_TEXTURE( pTexture );
+    SHADER_ALLOC_TEXTURE( pTexture );
+  }
+  
+}
