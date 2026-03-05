dfd43049dc5d391719a295cca07dca77a5e97787
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-05 10:11:39 -0600

    Update npc_node_size.cpp

--- engine/ai/npc_node_size.cpp
+++ engine/ai/npc_node_size.cpp
@@ -1,5 +1,5 @@
 /*npc_node_size.cpp*/
 
-static std::size_t _component::Npc::NpcNodeSize( Npc& pNpc ) {
-
+static std::size_t _component::Npc::NpcNodeSize( const Npc& pNpc ) {
+  return sizeof( pNpc );
 }
