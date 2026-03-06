caf01e68977fa2889e1e5409be250d30fb82f6ca
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-06 13:32:40 -0600

    Update weather.h

--- engine/fx/weather.h
+++ engine/fx/weather.h
@@ -3,14 +3,14 @@
 #ifndef WEATHER_H
 #define WEATHER_H
 
-constexpr const char* const WEATHER_CLASS("FX::Weather");
+constexpr const char* const WEATHER_CLASS("Component::FX::Weather");
 
 #define WEATHER_FX_RAIN 0
 #define WEATHER_FX_SNOW 1
 #define WEATHER_FX_THUNDER 2
 #define WEATHER_FX_DUST 3
 
-class CFXTypeWeather {
+class CFXTypeWeather : public FX {
 public:
   CFXTypeWeather();
   ~CFXTypeWeather() = default;
