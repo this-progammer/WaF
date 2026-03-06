36ba7c2f40dbaf3e59c9e0fc41b921cc87a748dc
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-06 10:27:45 -0600

    Update path_algorithm.h

--- engine/pathing/path_algorithm.h
+++ engine/pathing/path_algorithm.h
@@ -8,6 +8,12 @@ public:
   CPathAlgorithm();
   ~CPathAlgorithm() = default;
 
+virtual void* CreatePathAlgorithm( const CPathAlgorithm& pPath, const Node& pNodes, const Vector3& pGoto, const std::size_t& pSize, const int& nCount ) = 0;
+
+protected:
+Vector3 path_positioning;
+std::size_t path_size;
+int path_node_count;
 };
 
 #endif
