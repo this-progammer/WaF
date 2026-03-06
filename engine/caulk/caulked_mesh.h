36a1ef47a5cf77faa6e8ff47f0e30645087fb281
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-06 13:25:23 -0600

    Create caulked_mesh.h

---
+++ engine/caulk/caulked_mesh.h
@@ -0,0 +1,26 @@
+/*caulked_mesh.h*/
+
+#ifndef CAULKED_MESH_H
+#define CAULKED_MESH_H
+
+namespace _component {
+class CaulkedMesh : public Mesh {
+public:
+  CaulkedMesh( const Mesh& caulkedMesh );
+  ~CaulkedMesh() = default;
+
+virtual void CaulkMesh( const Mesh& mesh, const Texture& caulk ) = 0;
+virtual void UncaulkMesh( const Mesh& mesh, const Texture** prev_texture ) = 0;
+
+// get the caulked mesh
+CaulkedMesh& getMesh() {
+  return *this;
+}
+
+protected:
+ Mesh& mesh;
+ Texture& caulk_texture;
+};
+}
+
+#endif
