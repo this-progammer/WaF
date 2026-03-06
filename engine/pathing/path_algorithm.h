93bab3aa9e2745f9a25eb395c8b48da6c432c3b7
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-06 10:54:46 -0600

    Update path_algorithm.h

--- engine/pathing/path_algorithm.h
+++ engine/pathing/path_algorithm.h
@@ -9,9 +9,13 @@ public:
   ~CPathAlgorithm() = default;
 
 virtual void* CreatePathAlgorithm( const CPathAlgorithm& pPath, const Node& pNodes, const Vector3& pGoto, const std::size_t& pSize, const int& nCount ) = 0;
+virtual void* DestroyPath( CPathAlgorithm& pPath ) = 0;
+
+virtual void SetPathName( const CPathAlgorithm& pPath, const char* pName ) = 0;
 
 protected:
 Vector3 path_positioning;
+const char* path_name;
 std::size_t path_size;
 int path_node_count;
 };
